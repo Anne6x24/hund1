e0fcc7cffd9cfbac99a3d828212a83223397a118
ceci919p <ceci919p@stud.kea.dk>
2021-09-13 16:02:57 +0200

    kode kommentarer added

--- burgermenu.js
+++ burgermenu.js
@@ -2,18 +2,20 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
   console.log("sidenVises");
-  document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+  document.querySelector("#menuknap").addEventListener("click", toggleMenu); //lyt efter klik
 }
 
 function toggleMenu() {
   console.log("toggleMenu");
-  document.querySelector("#menu").classList.toggle("hidden");
+  document.querySelector("#menu").classList.toggle("hidden"); //toggle = fjern og tilføj skiftevis klassen
 
-  let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+  let erSkjult = document.querySelector("#menu").classList.contains("hidden"); //erSkjult er når menuen har klassen hidden
 
   if (erSkjult == true) {
+    //hvis det er sandt at menuen er skjult (har klassen hidden) skal dette gøres:
     document.querySelector("#menuknap").textContent = "☰";
   } else {
+    //hvis ikke menuen er skjult skal dette gøres:
     document.querySelector("#menuknap").textContent = "X";
   }
 }
--- script.js
+++ script.js
@@ -18,54 +18,57 @@ function start() {
   temp = document.querySelector("template");
 
   const filterKnapper = document.querySelectorAll("nav button");
-  filterKnapper.forEach((knap) => knap.addEventListener("click", filtrerHunde));
+  filterKnapper.forEach((knap) => knap.addEventListener("click", filtrerHunde)); //lyt efter klik
 
-  hentData();
+  hentData(); //JSON data
 }
 
 function filtrerHunde() {
-  filter = this.dataset.talent;
-  document.querySelector(".valgt").classList.remove("valgt");
+  filter = this.dataset.talent; //vi henter information til hvad filteret er.
+  document.querySelector(".valgt").classList.remove("valgt"); //vi fjerner klassen for at den kan tilføjes (der kan klikkes) igen.
   this.classList.add("valgt");
 
   visHund();
-  header.textContent = this.textContent;
+  header.textContent = this.textContent; //vores overskrift i filteret
 }
 
 async function hentData() {
-  const resspons = await fetch(url, options);
+  const resspons = await fetch(url, options); //vi henter vores JSON data.
   hunde = await resspons.json();
   //console.log(hunde);
   visHund();
 }
 
 function visHund() {
-  container.textContent = "";
+  container.textContent = ""; //vi tømmer vores container med data for at vi kan klikke og filtrere igen.
   console.log(filter);
   hunde.forEach((dyr) => {
     //  console.log(dyr.talent);
     if (filter == dyr.talent || filter == "alle") {
-      const klon = temp.cloneNode(true).content;
+      //hvis filter er lig med dyr.talent eller filter er lig med "alle" skal dettes gøres:
+      const klon = temp.cloneNode(true).content; //vores template skal klones
       const pic = ".jpg";
-      klon.querySelector("img").src = "billeder/" + dyr.billede + pic;
+      klon.querySelector("img").src = "billeder/" + dyr.billede + pic; //billedet i template - sæt data fra JSON ind.
       klon.querySelector("h3").textContent = dyr.navn;
       klon.querySelector(".alder").textContent = "Alder: " + dyr.alder;
       klon.querySelector(".race").textContent = "Race: " + dyr.race;
       klon.querySelector(".talent").textContent = "Talent: " + dyr.talent;
       klon
         .querySelector(".quickview")
-        .addEventListener("click", () => visDetaljer(dyr));
+        .addEventListener("click", () => visDetaljer(dyr)); //lyt efter klik
 
       klon.querySelector(".detaljer").addEventListener("click", () => {
+        //lyt efter klik og i stedet for function skal vi sendes videre til location.
         location.href = "singleview.html?id=" + dyr._id;
       });
 
-      container.appendChild(klon);
+      container.appendChild(klon); //afslutning på kloning
     }
   });
 }
 
 function visDetaljer(hundeData) {
+  //samme princip som visHund
   const popup = document.querySelector("#popup");
   popup.style.display = "block";
   popup.querySelector(".billede").src =
@@ -80,10 +83,10 @@ function visDetaljer(hundeData) {
   //console.log(hundeData);
 }
 
-document.querySelector("#tilbage").addEventListener("click", lukPopup);
+document.querySelector("#tilbage").addEventListener("click", lukPopup); //lyt efter klik
 
 function lukPopup() {
-  document.querySelector("#popup").style.display = "none";
+  document.querySelector("#popup").style.display = "none"; //fjerner popup
 }
 
-hentData();
+hentData(); //JSON data
